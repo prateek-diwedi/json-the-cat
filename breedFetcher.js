b65f5c8d8dc7035829790909c28b4857325fdc8d
Prateek Diwedi <diwedi1112@gmail.com>
2019-12-07 20:51:49 +0000

    final working app to find the description of any cat in the freaking world

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,25 @@
 const request = require('request');
-const input = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + input, function(error, response, body) {
-  if (error) {
-    console.log('error: unable to connect', error); // Print the error if one occurred
-  }
-  if (response) {
-  //console.log('statusCode: code for your request ', response && response.statusCode); // Print the response status code if a response was received
-  }
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  if (body) {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Breed not defined.');
-    } else {
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function(error, response, body) {
+ 
+    //console.log('body:', body); // Print the HTML for the Google homepage.
+    if (body) {
+      //console.log("Body");
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        console.log('Breed not defined.');
+        callback(error, 'Breed not defined');
+      } else {
+        //console.log("EMPTY");
       //console.log(data)
-      let summary = data[0].description;
-      console.log(summary);
+        let summary = data[0].description;
+        //console.log(summary);
+        callback(error, summary);
+      }
     }
-  }
-});
-//data[0].description
+  });
+};
+module.exports = fetchBreedDescription;
